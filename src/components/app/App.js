e106d446af12b99841e57f7bfac6820554735d2a
Lisa <example@gmail.com>
2023-10-04 18:42:19 +0600

    create CharInfo & use componentDidUpdate

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,3 +1,4 @@
+import { Component } from "react/cjs/react.production.min";
 import AppHeader from "../appHeader/AppHeader";
 import RandomChar from "../randomChar/RandomChar";
 import CharList from "../charList/CharList";
@@ -5,20 +6,33 @@ import CharInfo from "../charInfo/CharInfo";
 
 import decoration from '../../resources/img/vision.png';
 
-const App = () => {
-    return (
-        <div className="app">
-            <AppHeader/>
-            <main>
-                <RandomChar/>
-                <div className="char__content">
-                    <CharList/>
-                    <CharInfo/>
-                </div>
-                <img className="bg-decoration" src={decoration} alt="vision"/>
-            </main>
-        </div>
-    )
+class App extends Component {
+
+    state = {
+        selectedChar: null          /* 2. устанавливается сюда */
+    }
+
+    onCharSelected = (id) => {
+        this.setState({
+            selectedChar: id
+        })
+    }
+
+    render () {
+        return (
+            <div className="app">
+                <AppHeader/>
+                <main>
+                    <RandomChar/>
+                    <div className="char__content">
+                        <CharList onCharSelected={this.onCharSelected}/>        {/* 1. от сюда приходит айди*/}
+                        <CharInfo charId={this.state.selectedChar}/>            {/* 3. передаётся сюда */}
+                    </div>
+                    <img className="bg-decoration" src={decoration} alt="vision"/>
+                </main>
+            </div>
+        )
+    }
 }
 
 export default App;
